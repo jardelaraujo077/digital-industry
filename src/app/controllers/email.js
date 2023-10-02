a767463675aa961a0994a475e10a7b4b31bc8511
Jardel Araujo <jardel_1996github@hotmail.com>
2023-10-02 13:44:20 -0300

    ajustes

--- src/app/controllers/email.js
+++ src/app/controllers/email.js
@@ -2,22 +2,26 @@ const nodemailer =require("../../config/nodemailer")
 const funcao = require("../../function")
 exports.sendEmail = async (req,res) =>{ 
 let data = req.body;
+const allowedOrigin = 'https://www.digitalindustry.com.br';
+const origin = req.get('origin');
 
-
-let menssage = await funcao.sendEmailContact(data.nome, data.email, data.empresa, data.telefone, data.msg )  
-let resultadoEmail = await nodemailer.configEmail(menssage)  
-console.log('aqui',resultadoEmail)
-  if(!resultadoEmail){
-      res.status(400).json({
-          msg: "E-mail não enviado!!",
-          status: false
-      })
-  }
-res.status(200).json({
-        status: true,
-        msg: 'E-mail enviado com sucesso',
-    data: menssage
-    })
-
+if (origin === allowedOrigin) {
+    let menssage = await funcao.sendEmailContact(data.nome, data.email, data.empresa, data.telefone, data.msg )  
+    let resultadoEmail = await nodemailer.configEmail(menssage)  
+    console.log('aqui',resultadoEmail)
+      if(!resultadoEmail){
+          res.status(400).json({
+              msg: "E-mail não enviado!!",
+              status: false
+          })
+      }
+    res.status(200).json({
+            status: true,
+            msg: 'E-mail enviado com sucesso',
+        data: menssage
+        })
+} else {
+  res.status(403).json({ erro: 'Acesso não permitido' });
+}
 }
 
--- src/config/express.js
+++ src/config/express.js
@@ -3,21 +3,8 @@ const app = express();
 const routes = require('../app/routes')
 const bodyParser = require('body-parser');
 cors = require('cors')
-const allowedOrigin = 'https://www.digitalindustry.com.br'; 
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (origin === allowedOrigin || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Acesso não permitido por CORS'));
-    }
-  },
-};
-
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cors(corsOptions));
-//app.use(cors(corsOptions))
+app.use(cors())
 //acessando as rotas declaradas.
 app.use('/', routes)
 
