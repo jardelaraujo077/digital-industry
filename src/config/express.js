21eddfaf92b7a93f30037fda677bcfbeea439d87
Jardel Araujo <jardel_1996github@hotmail.com>
2023-10-02 13:35:40 -0300

    ajustes

--- src/config/express.js
+++ src/config/express.js
@@ -3,8 +3,21 @@ const app = express();
 const routes = require('../app/routes')
 const bodyParser = require('body-parser');
 cors = require('cors')
+const allowedOrigin = 'https://www.digitalindustry.com.br'; 
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (origin === allowedOrigin || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Acesso não permitido por CORS'));
+    }
+  },
+};
+
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cors())
+app.use(cors(corsOptions));
+//app.use(cors(corsOptions))
 //acessando as rotas declaradas.
 app.use('/', routes)
 
